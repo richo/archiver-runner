0ad08b026c0a4eaa469700bd22ce6b2402a30ee5
Richo Healey <richo@psych0tik.net>
2019-06-22 15:29:34 -0700

    Mostly working prototype

--- archiver-runner/AppDelegate.swift
+++ archiver-runner/AppDelegate.swift
@@ -8,20 +8,96 @@
 
 import Cocoa
 
+// TODO(richo) Make the user open their config manually so that we can use the files permission apple wants.
+
+
+enum RunState {
+    case Running
+    case Stopped
+}
+// TODO(richo) something something resources something something build step
+var executableURL = URL(fileURLWithPath: "/Users/richo/code/ext/archiver/target/debug/runner")
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    var child: Process?
+    var state: RunState = RunState.Stopped
+    
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var buttan: NSButton!
+    
+    @IBAction func stopStartButton(_ sender: Any) {
+        switch state {
+        case .Stopped:
+            child = createProcess()
+            
 
+            let stdout = Pipe()
+            let stderr = Pipe()
 
+            child?.standardOutput = stdout
+            child?.standardError = stderr
+
+            child?.terminationHandler = { process in
+                stdout.fileHandleForReading.readabilityHandler = nil
+                stderr.fileHandleForReading.readabilityHandler = nil
+
+                // TODO(richo) Deal with resetting the ui.
+            }
+            
+
+            
+            do {
+                try child?.run()
+            } catch {
+                print("Well that didn't work: \(error).")
+            }
+            stdout.fileHandleForReading.readabilityHandler = { pipe in
+                if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
+                    DispatchQueue.main.async {
+                        self.textOutput?.textStorage?.mutableString.append(line)
+                        print("output \(line)")
+                    }
+                } else {
+                    print("Error decoding data: \(pipe.availableData)")
+                }
+            }
+            stderr.fileHandleForReading.readabilityHandler = { pipe in
+                if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
+                    DispatchQueue.main.async {
+                        self.textOutput?.textStorage?.mutableString.append(line)
+                        print("error \(line)")
+                    }
+                } else {
+                    print("Error decoding data: \(pipe.availableData)")
+                }
+            }
+            
+            buttan.title = "Stop"
+            state = .Running
+        case .Running:
+            buttan.title = "Start"
+            state = .Stopped
+        }
+        
+    }
+    @IBOutlet var textOutput: NSTextView!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSLog("Started")
+        self.textOutput?.insertText("butts")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        NSLog("Bye")
     }
-
-
+    
+    fileprivate func createProcess() -> Process {
+        let task = Process()
+        task.executableURL = executableURL
+        task.arguments = []
+        
+        return task
+    }
 }
 
